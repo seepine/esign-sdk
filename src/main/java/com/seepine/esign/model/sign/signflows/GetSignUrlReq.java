3484c398e1df40cc044bf201a4ca3b89648b5f6a
seepine <seepine@outlook.com>
2023-02-21 10:11:48 +0800

    tag 1.1.3 增加获取签署地址传参

--- src/main/java/com/seepine/esign/model/sign/signflows/GetSignUrlReq.java
+++ src/main/java/com/seepine/esign/model/sign/signflows/GetSignUrlReq.java
@@ -1,6 +1,7 @@
 package com.seepine.esign.model.sign.signflows;
 
 import cn.hutool.http.Method;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.seepine.esign.common.http.Request;
 import lombok.Data;
@@ -11,15 +12,23 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GetSignUrlReq extends Request {
-  String flowId;
+  @JsonIgnore String flowId;
+  @JsonIgnore String accountId;
+  @JsonIgnore String organizeId;
 
-  public GetSignUrlReq(String flowId) {
+  public GetSignUrlReq(String flowId, String accountId, String organizeId) {
     this.flowId = flowId;
+    this.accountId = accountId;
+    this.organizeId = organizeId;
   }
 
   @Override
   public void build() {
-    setUrl("/v1/signflows/" + flowId + "/executeUrl");
+    String url = "/v1/signflows/" + flowId + "/executeUrl?accountId=" + accountId;
+    if (organizeId != null) {
+      url += "&organizeId=" + organizeId;
+    }
+    setUrl(url);
     setMethod(Method.GET);
   }
 }
